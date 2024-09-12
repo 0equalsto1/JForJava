00fb5991220a1f6d3001060f271139bf406b4f95
mannu <0equalsto1@gmail.com>
2024-09-12 17:11:31 +0530

    up

---
+++ src/main/java/JavaLogo.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+
+public class JavaLogo {
+    static int tree;
+    static String treeLine;
+    static {
+        tree = 0x1F333;
+        treeLine = Character.toString(tree).repeat(51);
+        Random random = new Random();
+        int nextInt = random.nextInt(10);
+        if (nextInt < 4) {
+            System.out.println("   |  /\\ \\  / /\\  \n" +
+                    " \\_| /--\\ \\/ /--\\ ");
+        } else if (nextInt < 8) {
+            System.out.println("""
+                         ,--. ,---.,--.   ,--.,---.  \s
+                         |  |/  O  \\\\  `.'  //  O  \\ \s
+                    ,--. |  |  .-.  |\\     /|  .-.  |\s
+                    |  '-'  /  | |  | \\   / |  | |  |\s
+                     `-----'`--' `--'  `-'  `--' `--'\s""");
+        } else {
+            System.out.println("""
+                       __     ______     __   __   ______   \s
+                      /\\ \\   /\\  __ \\   /\\ \\ / /  /\\  __ \\  \s
+                     _\\_\\ \\  \\ \\  __ \\  \\ \\ \\'/   \\ \\  __ \\ \s
+                    /\\_____\\  \\ \\_\\ \\_\\  \\ \\__|    \\ \\_\\ \\_\\\s
+                    \\/_____/   \\/_/\\/_/   \\/_/      \\/_/\\/_/\s""");
+        }
+
+        treeLine();
+    }
+
+    private static void treeLine() {
+        System.out.println(treeLine);
+    }
+}
---
+++ src/main/java/stringPoolAndIntern/StringInterningDiagnosticTest.java
@@ -0,0 +1,81 @@
+package stringPoolAndIntern;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+public class StringInterningDiagnosticTest {
+    public static final Random rand = new Random();
+    public static String STRING_TABLE_STATISTICS = "jcmd \"%s\" VM.stringtable|grep -m 1 'Number of literals'";
+    public static String STRING_TABLE_KEY_VALUE = "jcmd \"%s\" VM.stringtable -verbose|grep 'localString1'";
+    public static String[] statisticsCmd;
+    public static String[] printKeyValueCmd;
+
+    @BeforeAll
+    static void setUp() throws ClassNotFoundException {
+        Class.forName("JavaLogo");
+        String bash = "C:/Program Files/Git/bin/bash.exe";
+        long pid = getPid();
+        System.out.println("pid : " + pid);
+        STRING_TABLE_STATISTICS = String.format(STRING_TABLE_STATISTICS, pid);
+        STRING_TABLE_KEY_VALUE = String.format(STRING_TABLE_KEY_VALUE, pid);
+        String option = "-c";//It tells the shell to read and execute the command(s) from the string that follows, rather than from a script file or interactive input.
+        statisticsCmd = new String[]{bash, option, STRING_TABLE_STATISTICS};
+        printKeyValueCmd = new String[]{bash, option, STRING_TABLE_KEY_VALUE};
+    }
+
+    @Test
+    public void externalSourceStringLiteralWillNotAddedToPoolAutomatically() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+                System.out.println(line.intern());
+            }
+        } catch (IOException _) {
+        }
+    }
+
+    @Test
+    public void dynamicStringLiteralWillNotAddedToPoolAutomatically1() {
+        jcmdUtility(statisticsCmd);
+        jcmdUtility(printKeyValueCmd);
+        String literal = "localString1";
+        jcmdUtility(printKeyValueCmd);
+        literal = "localString" + rand.nextInt(2, 3);
+        jcmdUtility(printKeyValueCmd);
+    }
+
+    @Test
+    public void hash() {
+        System.out.println("8859_15".hashCode() & (127));
+        System.out.println("ibm-866".hashCode() & (127));
+    }
+
+    public void jcmdUtility(String[] commands) {
+        try {
+            ProcessBuilder builder = new ProcessBuilder(commands);
+            builder.redirectErrorStream(true);
+            Process process = builder.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            process.waitFor();
+
+        } catch (Exception _) {
+        }
+    }
+
+    private static long getPid() {
+        return ProcessHandle.current().pid();
+    }
+
+
+}
